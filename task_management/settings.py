4959b0a7e8ae9d294219069b59a703ef719ba2cc
mehrnoosh <mehrnooshdadashi@gmail.com>
2024-10-25 00:34:35 +0330

    import auth module from drf in settings.py

--- task_management/settings.py
+++ task_management/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from rest_framework import authentication
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
